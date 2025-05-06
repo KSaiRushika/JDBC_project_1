9521f02b8571561edc9f93e8d74675e64460ada1
KSaiRushika <146817980+KSaiRushika@users.noreply.github.com>
2025-05-06 09:27:23 +0530

    Program_1.java

--- src/com/test/Program_1.java
+++ src/com/test/Program_1.java
@@ -1,10 +1,12 @@
 package com.test;
-
+import java.util.*;
 public class Program_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n=sc.nextInt();
+		System.out.println("The square of the given number is: "+(n*n));
 	}
 
 }
